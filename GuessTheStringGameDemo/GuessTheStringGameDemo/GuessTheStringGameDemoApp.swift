51757eecf6e2d81eb0aa343527b97cd5fd508cda
Aleksandar Geyman <a.geyman@tinqin.com>
2025-11-07 23:47:35 +0200

    initial commit

---
+++ GuessTheStringGameDemo/GuessTheStringGameDemo/GuessTheStringGameDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheStringGameDemoApp.swift
+//  GuessTheStringGameDemo
+//
+//  Created by Aleksandar Geyman on 7.11.25.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheStringGameDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
