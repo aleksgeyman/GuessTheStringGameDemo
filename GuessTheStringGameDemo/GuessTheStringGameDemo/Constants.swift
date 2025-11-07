14cd3e16a202e8fac139d36f14d9d64ac1339942
Aleksandar Geyman <a.geyman@tinqin.com>
2025-11-07 23:57:56 +0200

    add constants, create logic to generate random string

---
+++ GuessTheStringGameDemo/GuessTheStringGameDemo/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  GuessTheStringGameDemo
+//
+//  Created by Aleksandar Geyman on 7.11.25.
+//
+
+import Foundation
+
+struct Constants {
+    static let GAME_TIME = 60
+    static let STRING_POOL = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    static let RANDOM_STRING_LENGTH = 4
+}
---
+++ GuessTheStringGameDemo/GuessTheStringGameDemo/Utils.swift
@@ -0,0 +1,18 @@
+//
+//  Utils.swift
+//  GuessTheStringGameDemo
+//
+//  Created by Aleksandar Geyman on 7.11.25.
+//
+
+import Foundation
+
+struct Utils {
+    
+    static func generateRandomString(
+        from stringPool: String = Constants.STRING_POOL,
+    length: Int = Constants.RANDOM_STRING_LENGTH
+    ) -> String {
+        String(stringPool.shuffled().prefix(length))
+    }
+}
